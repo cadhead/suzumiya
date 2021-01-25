6ff9efdcb4457581e1320be80691037f756081ec
cadhead <nekorides@gmail.com>
2021-01-26 02:05:46 +0300

    heroku setup posibility

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const server = http.createServer(app);
 
 const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 3000;
 const host = process.env.HOST || 'localhost';
+const appInfo = `${APP_NAME} v.${APP_VERSION}`;
 
 function getDomain(addr) {
   return typeof addr === 'string'
@@ -23,26 +24,58 @@ function getDomain(addr) {
     : `http://${addr.address}:${addr.port}/`;
 }
 
-io.attach(server);
-server.listen(port, (error) => {
+const getMemoryUsageInfo = () => {
+  const memoryUsageFormat = a => Math.round(((a / 1024 / 1024) * 100) / 100);
+  const memUsed = memoryUsageFormat(process.memoryUsage().heapUsed);
+  const memTotal = memoryUsageFormat(os.freemem());
+
+  return { memUsed, memTotal };
+};
+
+function localSetup(error) {
   if (error) {
     throw new Error(error);
   }
 
-  const memoryUsageFormat = a => Math.round(((a / 1024 / 1024) * 100) / 100);
-  const memUsed = memoryUsageFormat(process.memoryUsage().heapUsed);
-  const memTotal = memoryUsageFormat(os.freemem());
-  const appInfo = `${APP_NAME} v.${APP_VERSION}`;
+  const { memUsed, memTotal } = getMemoryUsageInfo();
+  const printInDevMode = (...mess) => {
+    if (process.env.NODE_ENV === 'development') {
+      console.info(...mess);
+    }
+  };
 
   console.info('');
   console.info(`\x1b[32m${appInfo} running successfully.\x1b[0m`);
   console.info('');
-  // console.info('\x1b[1mListening on:\x1b[34m', getDomain(server.address()), '\x1b[0m');
+  printInDevMode('\x1b[1mListening on:\x1b[34m', getDomain(server.address()), '\x1b[0m');
   console.info('\x1b[1mMemory usage:\x1b[0m', `${memUsed} / ${memTotal} MB`);
-  if (process.env.NODE_ENV === 'development') {
-    console.info('');
-    console.info('This program comes with \x1b[4mabsolutely no warranty\x1b[0m.');
-    console.info('This is free software, and you are welcome to redistribute it under certain conditions.');
-  }
+  printInDevMode('');
+  printInDevMode('This program comes with \x1b[4mabsolutely no warranty\x1b[0m.');
+  printInDevMode('This is free software, and you are welcome to redistribute it under certain conditions.');
   console.info('');
-});
+}
+
+function herokuSetup(error) {
+  if (error) {
+    throw new Error(error);
+  }
+
+  const { memUsed, memTotal } = getMemoryUsageInfo();
+
+  console.info(`${appInfo} successfully running on heroku.`);
+  console.info('Memory usage:', `${memUsed} / ${memTotal} MB`);
+}
+
+io.attach(server);
+
+switch (process.env.NODE_ENV) {
+  case 'heroku': {
+    server.listen(port, herokuSetup);
+
+    break;
+  }
+
+  default: {
+    server.listen(port, host, localSetup);
+  }
+}
